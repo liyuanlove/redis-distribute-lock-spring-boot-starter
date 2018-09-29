94d159295518eb593a607aa5aed239c892bb846f
姚康明 <154518484@qq.com>
2018-09-29 10:05:23 +0800

    optimise.

--- src/main/java/com/moonsinfo/redis/distribute/lock/spring/boot/autoconfigure/RedisDistributeLockAutoConfiguration.java
+++ src/main/java/com/moonsinfo/redis/distribute/lock/spring/boot/autoconfigure/RedisDistributeLockAutoConfiguration.java
@@ -4,14 +4,10 @@ import com.moonsinfo.redis.distribute.lock.spring.boot.autoconfigure.lock.Distri
 import com.moonsinfo.redis.distribute.lock.spring.boot.autoconfigure.lock.RedisDistributeLock;
 import com.moonsinfo.redis.distribute.lock.spring.boot.autoconfigure.lock.RedisLockAspect;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 
